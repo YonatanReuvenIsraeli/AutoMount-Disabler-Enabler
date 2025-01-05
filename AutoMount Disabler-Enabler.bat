f345a661f667b694c9bffa0bc17be83fd1270931
Yonatan Israeli <yonirisraeli@gmail.com>
2025-01-04 22:40:02 -0500

    Update AutoMount Disabler-Enabler.bat

--- AutoMount Disabler-Enabler.bat
+++ AutoMount Disabler-Enabler.bat
@@ -2,7 +2,7 @@
 setlocal
 title AutoMount Disabler/Enabler
 echo Program Name: AutoMount Disabler/Enabler
-echo Version: 1.2.19
+echo Version: 1.2.20
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
@@ -45,28 +45,28 @@ if /i "%AutoMount%"=="1" goto "1"
 if /i "%AutoMount%"=="2" goto "2"
 
 :"1"
-if exist "%cd%\DiskPart.txt" goto "DiskPartExist"
+if exist "%cd%\diskpart.txt" goto "DiskPartExist"
 echo.
 echo Disabling auto-mounting of new drives.
-(echo automount disable) > "%cd%\DiskPart.txt"
-(echo automount scrub) >> "%cd%\DiskPart.txt"
-(echo exit) >> "%cd%\DiskPart.txt"
-"%windir%\System32\diskpart.exe" /s "%cd%\DiskPart.txt" > nul 2>&1
+(echo automount disable) > "%cd%\diskpart.txt"
+(echo automount scrub) >> "%cd%\diskpart.txt"
+(echo exit) >> "%cd%\diskpart.txt"
+"%windir%\System32\diskpart.exe" /s "%cd%\diskpart.txt" > nul 2>&1
 if not "%errorlevel%"=="0" goto "DiskPartError"
-del "%cd%\DiskPart.txt" /f /q > nul 2>&1
+del "%cd%\diskpart.txt" /f /q > nul 2>&1
 echo Auto-mounting of new drives has been disabled.
 if /i "%DiskPart%"=="True" goto "DiskPartDone"
 goto "Start"
 
 :"2"
-if exist "%cd%\DiskPart.txt" goto "DiskPartExist"
+if exist "%cd%\diskpart.txt" goto "DiskPartExist"
 echo.
 echo Enabling auto-mounting of new drives.
-(echo automount enable) > "%cd%\DiskPart.txt"
-(echo exit) >> "%cd%\DiskPart.txt"
-"%windir%\System32\diskpart.exe" /s "%cd%\DiskPart.txt" > nul 2>&1
+(echo automount enable) > "%cd%\diskpart.txt"
+(echo exit) >> "%cd%\diskpart.txt"
+"%windir%\System32\diskpart.exe" /s "%cd%\diskpart.txt" > nul 2>&1
 if not "%errorlevel%"=="0" goto "DiskPartError"
-del "%cd%\DiskPart.txt" /f /q > nul 2>&1
+del "%cd%\diskpart.txt" /f /q > nul 2>&1
 echo Auto-mounting of new drives has been enabled.
 if /i "%DiskPart%"=="True" goto "DiskPartDone"
 goto "Start"
@@ -74,13 +74,13 @@ goto "Start"
 :"DiskPartExist"
 set DiskPart=True
 echo.
-echo Please temporary rename to something else or temporary move to another location "%cd%\DiskPart.txt" in order for this batch file to proceed. "%cd%\DiskPart.txt" is not a system file. Press any key to continue when "%cd%\DiskPart.txt" is renamed to something else or moved to another location. This batch file will let you know when you can rename it back to its original name or move it back to its original location.
+echo Please temporary rename to something else or temporary move to another location "%cd%\diskpart.txt" in order for this batch file to proceed. "%cd%\diskpart.txt" is not a system file. Press any key to continue when "%cd%\diskpart.txt" is renamed to something else or moved to another location. This batch file will let you know when you can rename it back to its original name or move it back to its original location.
 pause > nul 2>&1
 if /i "%AutoMount%"=="1" goto "1"
 if /i "%AutoMount%"=="2" goto "2"
 
 :"DiskPartError"
-del "%cd%\DiskPart.txt" /f /q > nul 2>&1
+del "%cd%\diskpart.txt" /f /q > nul 2>&1
 echo There has been an error! Press any key to try again.
 pause > nul 2>&1
 if /i "%AutoMount%"=="1" goto "1"
@@ -88,7 +88,7 @@ if /i "%AutoMount%"=="2" goto "2"
 
 :"DiskPartDone"
 echo.
-echo You can now rename or move back the file back to "%cd%\DiskPart.txt".
+echo You can now rename or move back the file back to "%cd%\diskpart.txt".
 goto "Start"
 
 :"Close"
