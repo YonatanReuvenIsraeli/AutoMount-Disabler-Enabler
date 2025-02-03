e2f9a12d4a7f740f547afed9a59c6c1a575561b8
Yonatan Israeli <yonirisraeli@gmail.com>
2025-02-03 15:13:51 -0500

    Update AutoMount Viewer-Disabler-Enabler.bat

--- AutoMount Viewer-Disabler-Enabler.bat
+++ AutoMount Viewer-Disabler-Enabler.bat
@@ -2,7 +2,7 @@
 setlocal
 title AutoMount Viewer/Disabler/Enabler
 echo Program Name: AutoMount Viewer/Disabler/Enabler
-echo Version: 2.0.9
+echo Version: 2.0.10
 echo License: GNU General Public License v3.0
 echo Developer: @YonatanReuvenIsraeli
 echo GitHub: https://github.com/YonatanReuvenIsraeli
